2664f0a59c4590e94e594c8743b424cdf6b6d03d
Chayney <chunchunmaru33333@gmail.com>
2025-06-28 16:17:42 +0900

    todoリスト作成

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 // ReactとReactDOMをインポート
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Todo } from './Todo.jsx';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
